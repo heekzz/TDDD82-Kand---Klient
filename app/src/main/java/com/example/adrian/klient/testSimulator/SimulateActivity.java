6f2c0fd92b652f27e25d05d285a2178f0cedf556
InspectorDeno <dennisdufback@gmail.com>
2016-04-15 10:21:14 +0200

    Simulator classes, file sender classes etc.

---
+++ app/src/main/java/com/example/adrian/klient/testSimulator/SimulateActivity.java
@@ -0,0 +1,82 @@
+package com.example.adrian.klient.testSimulator;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.adrian.klient.R;
+
+public class SimulateActivity extends AppCompatActivity {
+
+    final Simulator s = new Simulator(SimulateActivity.this);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_simulate);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Button addSmall = (Button) findViewById(R.id.simulateSmall);
+        Button addMedium = (Button) findViewById(R.id.simulateMedium);
+        Button addLarge = (Button) findViewById(R.id.simulateLarge);
+        Button delete = (Button) findViewById(R.id.deleteAll);
+
+        Button fileSmall = (Button) findViewById(R.id.smallFile);
+        Button fileMedium = (Button) findViewById(R.id.mediumFile);
+        Button fileLarge = (Button) findViewById(R.id.largeFile);
+        addSmall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                s.runSmall();
+            }
+        });
+        addMedium.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                s.runMedium();
+            }
+        });
+        addLarge.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                s.runLarge();
+            }
+        });
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                s.delete();
+            }
+        });
+        fileSmall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                s.sendSmall();
+            }
+        });
+        fileMedium.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                s.sendMedium();
+            }
+        });
+        fileLarge.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                s.sendLarge();
+            }
+        });
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+    }
+
+}
---
+++ app/src/main/java/com/example/adrian/klient/testSimulator/Simulator.java
@@ -0,0 +1,182 @@
+package com.example.adrian.klient.testSimulator;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.adrian.klient.R;
+import com.example.adrian.klient.ServerConnection.Connection;
+import com.example.adrian.klient.ServerConnection.FileConnection;
+import com.example.adrian.klient.ServerConnection.Request;
+import com.google.gson.JsonObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+
+/**
+ * Created by dennisdufback on 16-04-14.
+ */
+public class Simulator extends AppCompatActivity{
+    private ArrayList addList = new ArrayList();
+    private ArrayList deleteSmall = new ArrayList();
+    private ArrayList deleteMedium = new ArrayList();
+    private ArrayList deleteLarge = new ArrayList();
+
+    private Context context;
+    double lat = 58.8963790165493;
+    double lon = 15.460723824799063;
+    String event = "Auto";
+    Connection connection;
+    FileConnection fileConnection;
+
+    String smallFile,mediumFile,largeFile;
+    byte[] toSend;
+
+    public Simulator(Context context) {
+        this.context = context;
+    }
+
+    public void runSmall() {
+
+        for(int i =0; i < 5; i++) {
+            JsonObject toAdd = new JsonObject();
+            toAdd.addProperty("lat", lat);
+            toAdd.addProperty("lon", lon);
+            toAdd.addProperty("event", event);
+            addList.add(toAdd);
+
+            lat += 0.01;
+        }
+        deleteSmall = addList;
+
+        Request addRequest = new Request(context,"add",addList).mapRequest();
+        connection = new Connection(addRequest, context);
+        Thread t = new Thread(connection);
+        t.start();
+    }
+
+    public void runMedium() {
+        for (int i = 0; i < 50; i++) {
+            JsonObject toAdd = new JsonObject();
+            toAdd.addProperty("lat", lat);
+            toAdd.addProperty("lon", lon);
+            toAdd.addProperty("event", event);
+            addList.add(toAdd);
+            lat += 0.01;
+        }
+        deleteMedium = addList;
+        Request addRequest = new Request(context,"add",addList).mapRequest();
+        connection = new Connection(addRequest, context);
+        Thread t = new Thread(connection);
+        t.start();
+    }
+    public void runLarge() {
+
+        lon += 0.3;
+        for (int i = 0; i < 500; i++) {
+            JsonObject toAdd = new JsonObject();
+            toAdd.addProperty("lat", lat);
+            toAdd.addProperty("lon", lon);
+            toAdd.addProperty("event", event);
+            addList.add(toAdd);
+
+            lat += 0.01;
+        }
+        deleteLarge = addList;
+
+        Request addRequest = new Request(context,"add",addList).mapRequest();
+        connection = new Connection(addRequest, context);
+        Thread t = new Thread(connection);
+        t.start();
+
+    }
+    public void delete() {
+
+        if(!deleteSmall.isEmpty()) {
+            Request deleteRequest = new Request(context, "delete", deleteSmall).mapRequest();
+            connection = new Connection(deleteRequest, context);
+            Thread t = new Thread(connection);
+            t.start();
+        }
+        if(!deleteMedium.isEmpty()) {
+            Request deleteRequest = new Request(context, "delete", deleteMedium).mapRequest();
+            connection = new Connection(deleteRequest, context);
+            Thread t = new Thread(connection);
+            t.start();
+        }
+        if(!deleteLarge.isEmpty()) {
+            Request deleteRequest = new Request(context, "delete", deleteLarge).mapRequest();
+            connection = new Connection(deleteRequest, context);
+            Thread t = new Thread(connection);
+            t.start();
+        }
+
+    }
+
+    public void sendSmall(){
+        try {
+            toSend = loadFile("small");
+            fileConnection = new FileConnection(toSend,context);
+            Thread t = new Thread(fileConnection);
+            t.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void sendMedium(){
+        try {
+            toSend = loadFile("medium");
+            fileConnection = new FileConnection(toSend,context);
+            Thread t = new Thread(fileConnection);
+            t.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+    public void sendLarge(){
+        try {
+            toSend = loadFile("large");
+            fileConnection = new FileConnection(toSend,context);
+            Thread t = new Thread(fileConnection);
+            t.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public byte[] loadFile(String fileSize) throws IOException{
+        InputStream iS = null;
+
+        switch (fileSize){
+            case "small":
+                //5 MB
+                iS = getResources().openRawResource(R.raw.smallFile);
+                break;
+            //10 MB
+            case "medium":
+                iS = getResources().openRawResource(R.raw.mediumFile);
+                break;
+            //20 MB
+            case "large":
+                iS = getResources().openRawResource(R.raw.largeFile);
+                break;
+        }
+
+        //create a buffer that has the same size as the InputStream
+        byte[] buffer = new byte[iS.available()];
+        //read the text file as a stream, into the buffer
+        iS.read(buffer);
+//        //create a output stream to write the buffer into
+//        ByteArrayOutputStream oS = new ByteArrayOutputStream();
+//        //write this buffer to the output stream
+//        oS.write(buffer);
+//        //Close the Input and Output streams
+//        oS.close();
+//        iS.close();
+//        return oS.toString();
+        return buffer;
+    }
+
+}
